e12a3dcee0be2be9fe66e96af693e23ec31337e2
YONG Ye <yongye@YONGdeMacBook-Pro.local>
2026-08-30 00:20:00 +0800

    Add public TestFlight download link

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 // 發布前把網址換成 Google Play 封閉測試與 TestFlight 公開邀請連結。
 window.GAME_LINKS = {
   android: 'https://play.google.com/apps/testing/com.yongye.taiwanbasketballgm',
-  ios: 'https://testflight.apple.com/join/REPLACE_ME'
+  ios: 'https://testflight.apple.com/join/vdDt2uwZ'
 };
